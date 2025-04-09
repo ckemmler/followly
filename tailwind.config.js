9f2fc218e5f619f537e1b9873f48666bdbdb5e53
Candide Kemmler <hi@candide.me>
2025-04-09 14:52:25 +0200

    ensure static site generation

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // App Router
+    "./src/pages/**/*.{js,ts,jsx,tsx}", // Pages Router
+    "./src/components/**/*.{js,ts,jsx,tsx}", // If you use shared components
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-cinzel)", "serif"],
+        body: ["var(--font-cormorant)", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};

60156215d8abac6ec634fab83bd7fa0a8994d88e
Candide Kemmler <hi@candide.me>
2025-05-30 15:06:59 +0200

    remove premature experiments

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,6 @@
 import article from './article'
 import snippet from './snippet'
 import video from './video'
-import frame from './frame'
-import script from './script'
-import scene from './scene'
 import internationalizedArraySlug from './fields/internationalizedArraySlug'
 
-export default [frame, scene, script, article, snippet, video, internationalizedArraySlug]
+export default [article, snippet, video, internationalizedArraySlug]
